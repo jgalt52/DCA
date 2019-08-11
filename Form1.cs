b38ab530cb0efe75c9be7218b179ecb104b9a61c
jgalt52 <42588645+jgalt52@users.noreply.github.com>
2019-08-11 11:02:00 -0600

    Add files via upload
    loads all crimes and puts them on the map with dif markers

--- Form1.cs
+++ Form1.cs
@@ -44,12 +44,18 @@ namespace DenverCrimeApp
 
         private void gMapControl2_Load(object sender, EventArgs e)
         {
+
+            // Basic Settings for greatmaps app
             gmap.MapProvider = GMapProviders.GoogleMap;
             gmap.Position = new PointLatLng(39.73686290, -104.99164970);
             gmap.MinZoom = 10;
             gmap.MaxZoom = 15;
             gmap.Zoom = 10;
 
+
+
+
+            // Read in the .csv and create a list of objects with correct paramatetrs
             var lines = File.ReadAllLines(@"C:\\Users\\The Machine\\Desktop\\DenverCrimeApp\\DenverCrimeApp\\crime.csv");
             var csv = lines.Select(x =>
             {
@@ -65,47 +71,100 @@ namespace DenverCrimeApp
 
 
 
+
+            // Create a new list of markers using the csv list created above
            List<GMarkerGoogle> gmaplist = csv.Select(x =>
             {
 
+                //lets set the color type based crime type
+                if(x.GeoLat == ""|| x.GeoLong == "") {
+                    x.GeoLong = "0.0";
+                    x.GeoLat = "0.0";}
+
+
                 PointLatLng lambdapoint = new PointLatLng(Convert.ToDouble(x.GeoLat), Convert.ToDouble(x.GeoLong));
 
-                return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.red)
+                switch (x.OffenseCat)
+                {
+                    case "all-other-crimes":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.blue) { };
+
+                    case "larceny":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.brown_small) { };
+
+                    case "theft-from-motor-vehicle":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.green_big_go) { };
+
+                    case "traffic-accident":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.green) { };
+
+                    case "drug-alcohol":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.purple) { };
+
+                    case "auto-theft":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.orange_dot) { };
+
+
+                    case "white-collar-crime":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.white_small) { };
+
+                    case "burglary":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.yellow) { };
+
+                    case "public-disorder":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.blue_dot) { };
+
+                    case "aggravated-assault":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.orange) { };
+
+                    case "other-crimes-against-persons":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.orange_small) { };
+
+                    case "robbery":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.orange_dot) { };
+
+                    case "sexual-assault":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.red_dot) { };
+
+                    case "murder":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.red_big_stop) { };
+
+                    case "arson":
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.red_dot) { };
+
+                    default:
+                        return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.black_small) { };
+
+
+                }
+
+                //PointLatLng lambdapoint = new PointLatLng(Convert.ToDouble(x.GeoLat), Convert.ToDouble(x.GeoLong));
+
+                //return new GMarkerGoogle(lambdapoint, GMarkerGoogleType.red)
                 {
                     
                 };
 
             }).ToList(); 
             
-    
-            //MessageBox.Show(csv[2].objectspoint.ToString());
 
 
+
+            //Create an overlay
             GMapOverlay markers = new GMapOverlay("markers");
 
+
+
+            //Go through the marker list gmaplist and add them all to the layer
             foreach (GMarkerGoogle gmark in gmaplist)
             {
-               // MessageBox.Show(gmark.ToString());
+              
                         markers.Markers.Add(gmark);
-        
             }
 
             gmap.Overlays.Add(markers);
 
-
-            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(39.67510490, -104.99164970), GMarkerGoogleType.purple);
-            GMarkerGoogle marker2 = new GMarkerGoogle(new PointLatLng(39.67510490, -104.91057350), GMarkerGoogleType.blue);
-            GMarkerGoogle marker4 = new GMarkerGoogle(new PointLatLng(39.82405790, -104.77523330), GMarkerGoogleType.green);
-           // GMarkerGoogle marker3 = new GMarkerGoogle(new PointLatLng(latinput, longinput), GMarkerGoogleType.purple);
-            //markersOverlay.Markers.Add(marker);
-           // markersOverlay.Markers.Add(marker2);
-           // markersOverlay.Markers.Add(marker3);
-          //  markersOverlay.Markers.Add(marker4);
-           
-           
-      
-
-  
+           // GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(39.67510490, -104.99164970), GMarkerGoogleType.purple);
 
         }
     }
